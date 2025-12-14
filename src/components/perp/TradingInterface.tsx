49f32d6007c8c164169029468c6c53444dbbe43e
h8inc <aaterziev.c@gmail.com>
2025-12-14 10:18:19 +0200

    Set TradingBox column to ~30% on desktop (bounded width)

--- src/components/perp/TradingInterface.tsx
+++ src/components/perp/TradingInterface.tsx
@@ -396,7 +396,7 @@ export const TradingInterface = () => {
       
       <div className="flex flex-1 overflow-hidden flex-col lg:flex-row">
         {/* Left Column: Chart & Bottom Panel */}
-        <div className="flex w-full lg:flex-[0.58] flex-col min-w-0">
+        <div className="flex w-full lg:flex-[0.7] flex-col min-w-0">
           <ChartSection />
           <BottomPanel isWalletConnected={isWalletConnected} onConnectWallet={() => setIsWalletConnected(true)} />
         </div>
@@ -407,7 +407,7 @@ export const TradingInterface = () => {
         </div>
 
         {/* Right Column: Order Entry */}
-        <div className="hidden lg:block lg:flex-[0.42] lg:min-w-[420px] lg:max-w-none border-l border-white/10 bg-[#0b0e11] overflow-y-auto">
+        <div className="hidden lg:block lg:flex-[0.3] lg:min-w-[360px] lg:max-w-[520px] border-l border-white/10 bg-[#0b0e11] overflow-y-auto">
           <TradingBox isWalletConnected={isWalletConnected} onConnectWallet={() => setIsWalletConnected(true)} onDisconnect={() => setIsWalletConnected(false)} />
         </div>
       </div>
